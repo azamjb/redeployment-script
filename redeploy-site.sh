b986a5d7aae1e9e5eae52d46bae1245b46c37552
Azam Jawad Butt <85136312+azamjb@users.noreply.github.com>
2025-07-08 20:04:09 -0400

    modification for new deployment process

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-tmux kill-server
-
 cd 25.SUM.B3-Portfolio-Site
 
-git fetch
-
-git reset origin/main --hard
+git fetch && git reset origin/main --hard 
 
 source venv/bin/activate
 
 pip install -r requirements.txt
 
-tmux new -d -s portfolio 'cd 25.SUM.B3-Portfolio-Site && source venv/bin/activate && flask run --host=0.0.0.0'
+systemctl daemon reload
+
+systemctl restart myportfolio
